e27676063362250054044682cd4bcdc236b9bd47
Dennis Erdmann <erdmanndennismail@gmail.com>
2023-07-09 16:26:12 +0200

    changed foldername

--- pages/animals.js
+++ pages/animals.js
@@ -1,6 +1,6 @@
 import React from "react";
 import dogs from "../lib/data";
-import List from "../components/index.js";
+import List from "../components/animallist/index.js";
 
 const DogsPage = () => {
   return (
