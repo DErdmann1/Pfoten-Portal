b3276f5fcfac360cf1dbe4093543e66772296e4a
Dennis Erdmann <erdmanndennismail@gmail.com>
2023-07-10 10:28:44 +0200

    change from div to main

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,9 @@ import Link from "next/link";
 
 export default function HomePage() {
   return (
-    <div>
+    <main>
       <h1>🐾 PfotenPortal 🐾</h1>
       <Link href="/animals">Zur Pfotenseite </Link>
-    </div>
+    </main>
   );
 }
