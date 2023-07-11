20c14be47ebc8c5f88d19df26eafee1300642a65
Dennis Erdmann <erdmanndennismail@gmail.com>
2023-07-11 14:48:17 +0200

    create more detail components

---
+++ components/MoreDetailPages/DogMoreDetailsPage.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Image } from "react";
+import { useParams } from "react-router";
+import data from "../lib/data.js";
+
+function DogMoreDetailsPage() {
+  const { id } = useParams(); // Hier verwendest du die übergebene ID aus der URL
+
+  // Finde das entsprechende Tier in den Daten
+  const animal = data.find((item) => item.id === id);
+
+  if (!animal) {
+    return <Text>Tier nicht gefunden</Text>;
+  }
+
+  return (
+    <View>
+      <Image source={animal.image} alt={Bild desTieres} />
+      <Text>{animal.infoText}</Text>
+    </View>
+  );
+}
+
+export default DogMoreDetailsPage;
--- pages/moredetails.js
+++ pages/moredetails.js
@@ -1,1 +1,2 @@
 import React from "react";
+import { Image } from "react";
