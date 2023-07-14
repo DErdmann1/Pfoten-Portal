9e25ec8891480ef0f3482aeece41fe53580a27fa
Dennis Erdmann <erdmanndennismail@gmail.com>
2023-07-14 11:45:00 +0200

    create footer component

---
+++ components/Catlist/Footer/index.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const StyledFooter = styled.footer`
+  text-align: center;
+  border-top: 2px solid black;
+`;
+
+export default function Footer() {
+  return <StyledFooter>FOOTER</StyledFooter>;
+}
