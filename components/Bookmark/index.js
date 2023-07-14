27e4ce43466648c490fc4567e45b796653bb5b73
Dennis Erdmann <erdmanndennismail@gmail.com>
2023-07-14 14:36:43 +0200

    create bookmark index.js

---
+++ components/Bookmark/index.js
@@ -0,0 +1,37 @@
+// components/Bookmark/index.js
+import { useState } from "react";
+
+export default function BookmarkButton({ onBookmark }) {
+  const [isBookmarked, setIsBookmarked] = useState(false);
+
+  const handleClick = () => {
+    setIsBookmarked(!isBookmarked);
+    onBookmark(!isBookmarked);
+  };
+
+  return (
+    <button onClick={handleClick}>
+      {isBookmarked ? (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          width="24"
+          height="24"
+        >
+          <title>bookmark</title>
+          <path d="M17,3H7A2,2 0 0,0 5,5V21L12,18L19,21V5A2,2 0 0,0 17,3Z" />
+        </svg>
+      ) : (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          width="24"
+          height="24"
+        >
+          <title>bookmark_border</title>
+          <path d="M17,5V19L12,16.5L7,19V5A2,2 0 0,1 9,3H15A2,2 0 0,1 17,5M17,1H7A4,4 0 0,0 3,5V21L12,17.5L21,21V5A4,4 0 0,0 17,1Z" />
+        </svg>
+      )}
+    </button>
+  );
+}
