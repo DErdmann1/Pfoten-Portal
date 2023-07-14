cc0d5ca08a36f904a2507a13138a64eb081d2f93
Dennis Erdmann <erdmanndennismail@gmail.com>
2023-07-14 14:25:39 +0200

    add footer with icons

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,10 +1,37 @@
 import styled from "styled-components";
 
 const StyledFooter = styled.footer`
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 30px;
   text-align: center;
   border-top: 2px solid black;
 `;
 
 export default function Footer() {
-  return <StyledFooter>©PfotenPortal</StyledFooter>;
+  return (
+    <StyledFooter>
+      ©PfotenPortal
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        width="24"
+        height="24"
+      >
+        <title>home</title>
+        <path d="M10,20V14H14V20H19V12H22L12,3L2,12H5V20H10Z" />
+      </svg>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        width="24"
+        height="24"
+      >
+        <title>star</title>
+        <path d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z" />
+      </svg>
+    </StyledFooter>
+  );
 }
