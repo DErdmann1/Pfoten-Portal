13b07a5f70e087c0dd16ca7ec642dc7b6d397895
Dennis Erdmann <erdmanndennismail@gmail.com>
2023-07-18 14:42:49 +0200

    change width of header

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,65 @@
 import React from "react";
 import Link from "next/link";
 import Footer from "../components/Footer/index.js";
-import Header from "../components/Header";
 import styled from "styled-components";
+
+const StyledHeader = styled.header`
+  background-color: #987554;
+  width: 100%;
+  padding: 20px;
+  text-align: center;
+`;
+
+const Logo = styled.p`
+  font-size: 24px;
+  color: black;
+`;
 
 const StyledHomePage = styled.main`
   padding-bottom: 60px;
   background-color: #e5d3b3;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 const Button = styled.button`
-  display: block;
-  margin: 10px auto;
-  padding: 10px 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   background-color: #987554;
   color: white;
   border: none;
-  border-radius: 5px;
+  border-radius: 8px;
+  padding: 20px;
+  text-decoration: none;
+  font-weight: bold;
   font-size: 16px;
   cursor: pointer;
+  margin-top: 20px;
 `;
 
 export default function HomePage() {
   return (
-    <StyledHomePage>
-      <Header />
-      <h1>🐾 PfotenPortal 🐾</h1>
-      <Link href="/dogs">
-        <Button>Zu unseren Hunden</Button>
-      </Link>
-      <Link href="/cats">
-        <Button>Zu unseren Katzen</Button>
-      </Link>
-      <Link href="/smallanimals">
-        <Button>Zu unseren Kleintieren</Button>
-      </Link>
-      <Footer />
-    </StyledHomePage>
+    <>
+      <StyledHeader>
+        <Link href="/">
+          <Logo>🐾 PfotenPortal 🐾</Logo>
+        </Link>
+      </StyledHeader>
+      <StyledHomePage>
+        <h1>🐾 PfotenPortal 🐾</h1>
+        <Link href="/dogs">
+          <Button>Zu unseren Hunden</Button>
+        </Link>
+        <Link href="/cats">
+          <Button>Zu unseren Katzen</Button>
+        </Link>
+        <Link href="/smallanimals">
+          <Button>Zu unseren Kleintieren</Button>
+        </Link>
+        <Footer />
+      </StyledHomePage>
+    </>
   );
 }
