9e599a7ef6e9f461473a44e7232e782f86d1ced2
Dennis Erdmann <erdmanndennismail@gmail.com>
2023-07-26 12:33:35 +0200

    create dogicon component

---
+++ components/Icons/DogIcon.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const DogIcon = () => (
+  <svg
+    viewBox="0 0 64 64"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    aria-hidden="true"
+    role="img"
+    className="iconify iconify--emojione"
+    preserveAspectRatio="xMidYMid meet"
+    fill="#000000"
+    id="SVGRepo_bgCarrier"
+    strokeWidth="0"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    {/* ... (SVG code) */}
+  </svg>
+);
+
+export default DogIcon;
