342c1e6df9b607b160c2a3e610084ba9504dab4c
Dennis Erdmann <erdmanndennismail@gmail.com>
2023-07-28 15:33:13 +0200

    set border to animals

--- components/Smallanimallist/index.js
+++ components/Smallanimallist/index.js
@@ -20,6 +20,11 @@ const SmallanimalImage = styled(Image)`
 const ListItem = styled.li`
   display: flex;
   flex-direction: column;
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  padding: 10px;
+  margin: 10px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
 `;
 
 const ButtonContainer = styled.div`
