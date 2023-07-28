171ffcf669ced2ce158d829f2ad0262a568aa024
Dennis Erdmann <erdmanndennismail@gmail.com>
2023-07-28 15:42:27 +0200

    add top margin and padding

--- components/Doglist/index.js
+++ components/Doglist/index.js
@@ -22,8 +22,10 @@ const ListItem = styled.li`
   flex-direction: column;
   border: 1px solid #ddd;
   border-radius: 8px;
-  padding: 10px;
-  margin: 10px;
+  padding-left: 25px;
+  padding-right: 25px;
+  margin-top: 30px;
+  margin-bottom: 60px;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
 `;
 
